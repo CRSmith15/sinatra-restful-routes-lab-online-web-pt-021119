f3d176a1efe538472a240962b51fd8e3b39a01dc
Chance Smith <chancesmith97@hotmail.com>
2019-12-09 03:32:45 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,29 +8,31 @@ class ApplicationController < Sinatra::Base
     redirect '/recipes'
   end
   
-  get '/recipes' do 
-    @recipes = Recipe.all 
-    
-    erb :index
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index	    erb :index
+  end	  end
+
+  get '/recipes/new' do
+    erb :new
   end
-  
-  get '/recipes/new' do 
-    erb :new_recipe
-  end
-  
-  post '/recipes' do 
-    Recipe.new(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time]).tap do |recipe|
-        recipe.save
-      redirect '/recipes/#{recipe.id}'
+
+  post '/recipes' do
+    Recipe.new(
+      name: params[:name],
+      ingredients: params[:ingredients],
+      cook_time: params[:cook_time]
+    ).tap do |recipe|
+      recipe.save
+      redirect "/recipes/#{recipe.id}"
     end
   end
-  
-  get '/recipes/:id' do 
+
+  get '/recipes/:id' do
     @recipe = Recipe.find(params[:id])
-    
     erb :show
   end
-  
+
   get '/recipes/:id/edit' do
     @recipe = Recipe.find(params[:id])
     erb :edit
